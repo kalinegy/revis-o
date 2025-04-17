193851e40ef0e0df73b93a59a6596e06c2b91c84
kaline gabrielly <gkaline769@gmail.com>
2025-04-17 14:01:40 -0300

    teste1

---
+++ teste1.py
@@ -0,0 +1,11 @@
+a=float(input("Insira o valor de a:"))
+b=float(input("Insira o valor de b:"))
+c=float(input("Insira o valor de c:"))
+soma=a+b
+print(soma)
+if soma < c:
+    print("O valor da soma de a e b é menor que c")
+else:
+    print("O valor da soma de a e b NÃO é menor que c")
+
+
