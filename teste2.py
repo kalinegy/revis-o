304908b4bd884479e02eff292011ea77e5fd8cb8
kaline gabrielly <gkaline769@gmail.com>
2025-04-17 14:11:59 -0300

    teste2

---
+++ teste2.py
@@ -0,0 +1,9 @@
+num=float(input("Digite um numero:"))
+if num >= 0:
+    print(f"Este número {num} é positivo")
+else:
+     print(f"Este número {num} é negativo")
+if num %2 !=0:
+    print(f"Este número {num} é ímpar")
+else:
+    print(f"Este número {num} é par")
