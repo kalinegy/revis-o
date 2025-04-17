a1ecbda99c0faf45660b64de7edc9ef82c444c96
kaline gabrielly <gkaline769@gmail.com>
2025-04-17 14:26:25 -0300

    alteração de código2

--- teste2.py
+++ teste2.py
@@ -1,9 +1,12 @@
-num=float(input("Digite um numero:"))
-if num >= 0:
-    print(f"Este número {num} é positivo")
-else:
-     print(f"Este número {num} é negativo")
-if num %2 !=0:
-    print(f"Este número {num} é ímpar")
-else:
-    print(f"Este número {num} é par")
+resp="s"
+while resp == "s":
+    num=float(input("Digite um numero:"))
+    if num >= 0:
+        print(f"Este número {num} é positivo")
+    else:
+         print(f"Este número {num} é negativo")
+    if num %2 !=0:
+        print(f"Este número {num} é ímpar")
+    else:
+        print(f"Este número {num} é par")
+    resp= input("Deseja verificar um novo número [s/n]:")
